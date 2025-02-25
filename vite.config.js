07bf90c6e56a210035d142a3eec503ea0c07badf
Furquan Anwer <furquan.anwer@gmail.coom>
2025-02-25 21:50:13 +0530

    routing issue fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+// });
+
+
+
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(),eslint()],
+  server: {
+    historyApiFallback: true, // Ensures Vite's dev server supports SPA routing
+  },
+  build: {
+    outDir: 'dist', // Ensure it's set correctly
+  }
 });
